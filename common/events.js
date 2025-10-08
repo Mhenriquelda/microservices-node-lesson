9057f1fbf2998d37e0f48d3394c9a50e68ccdcfa
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2025-10-07 21:02:47 -0300

    atualizei o events.js

--- common/events.js
+++ common/events.js
@@ -1,6 +1,8 @@
-module.exports = {
-  ROUTING_KEYS: {
-    USER_CREATED: 'user.created',
-    ORDER_CREATED: 'order.created'
-  }
+// Exporta as constantes para serem usadas em outros módulos via 'import'.
+export const ROUTING_KEYS = {
+  // Evento disparado quando um novo usuário é criado.
+  USER_CREATED: 'user.created',
+
+  // Evento disparado quando um novo pedido é criado.
+  ORDER_CREATED: 'order.created'
 };
